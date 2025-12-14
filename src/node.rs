ff05c41808c3517e8de3b7a600a5dbee2b867259
Du Shiqiao <lucidfrontier.45@gmail.com>
2025-12-14 18:11:58 +0900

    feat: define LowerBound and Approximate traits for enhanced tree node cost functionality

--- src/node.rs
+++ src/node.rs
@@ -12,14 +12,24 @@ pub trait TreeNode: Sized {
     /// Returns the cost associated with the node, if any.
     /// If the current node does not satisfy problem constraints, returns None.
     fn cost(&self) -> Option<Self::Cost>;
+}
+
+/// Trait defining the lower bound functionality for tree nodes.
+pub trait LowerBound {
+    /// The type representing the cost associated with the node.
+    type Cost: Copy + Ord;
+
     /// Returns the lower bound associated with the node, if any.
     /// If the current node does not satisfy problem constraints, returns None.
-    fn cost_lb(&self) -> Option<Self::Cost> {
-        self.cost()
-    }
+    fn cost_lb(&self) -> Option<Self::Cost>;
+}
+
+/// Trait defining the approximate cost functionality for tree nodes.
+pub trait Approximate {
+    /// The type representing the cost associated with the node.
+    type Cost: Copy + Ord;
+
     /// Returns the approximate cost associated with the node, if any.
     /// If the current node does not satisfy problem constraints, returns None.
-    fn cost_approx(&self) -> Option<Self::Cost> {
-        self.cost()
-    }
+    fn cost_approx(&self) -> Option<Self::Cost>;
 }
